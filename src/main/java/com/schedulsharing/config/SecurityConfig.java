e8fca8ecb5295266b50478c99e559fc998d27335
donup3 <donup08@naver.com>
2021-03-08 10:25:25 +0900

    jenkins CI test1

--- src/main/java/com/schedulsharing/config/SecurityConfig.java
+++ src/main/java/com/schedulsharing/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .antMatchers("/").permitAll()
                 .antMatchers("/api/hello").permitAll()
                 .antMatchers("/api/authenticate").permitAll()
                 .antMatchers("/api/member/signup","/api/member/checkEmail").permitAll()

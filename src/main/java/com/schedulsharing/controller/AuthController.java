30d5f4fa4666afb505f6fd5854759c7057a99923
donup3 <donup08@naver.com>
2021-03-30 10:52:56 +0900

    jenkins CI/CD test6

--- src/main/java/com/schedulsharing/controller/AuthController.java
+++ src/main/java/com/schedulsharing/controller/AuthController.java
@@ -24,8 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
